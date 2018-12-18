667cf70697f896185855880448d314d092bccae4
xiaoyue8 <xiaoyue8@outlook.com>
2018-12-18 20:20:27 +0800

    登陆页面配置搭建：1.urls增加路由；2.settings:1)增加demo工程2)禁用csrf;3.views:1)login;2)UserForm;4.templates:1)login.html

--- demo/views.py
+++ demo/views.py
@@ -1,1 +1,21 @@
 # Create your views here.
+from django import forms
+from django.http import HttpResponse
+from django.shortcuts import render_to_response
+
+
+class UserForm(forms.Form):
+	username = forms.CharField()
+	password = forms.CharField(widget=forms.PasswordInput)
+
+def login(req):
+	if req.method == "POST":
+		uf = UserForm(req.POST)
+		if uf.is_valid():
+			username = uf.cleaned_data['username']
+			password = uf.cleaned_data['password']
+			print username,password
+			return HttpResponse('login ok')
+	else :
+		uf = UserForm()
+	return render_to_response('login.html',{'uf':uf})
